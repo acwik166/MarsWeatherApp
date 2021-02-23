08678b41489eb9e7d3b3be7dc850ad9a63fd4b8e
acwik166 <maciekposala11@gmail.com>
2021-02-23 13:04:40 +0100

    ap

--- src/context/WeatherContext.js
+++ src/context/WeatherContext.js
@@ -9,7 +9,7 @@ export const WeatherProvider = ({ children }) => {
     const [weatherData, setWeatherData] = useState();
 
     useEffect(() => {
-        const api_key = 'YOUR_API_KEY';
+        const api_key = '7e4cyaBOukW57RyBHeK8dff3woblCwgFmukr5zyN' 
         const url = `https://api.nasa.gov/insight_weather/?api_key=${api_key}&feedtype=json&ver=1.0`;
         fetch(url)
         .then((response) => response.json())
@@ -20,15 +20,15 @@ export const WeatherProvider = ({ children }) => {
                 const day = date.getUTCDate();
                 return { 
                     sol: sol_key,
-                    max_temperature: +data[sol_key].AT?.mx.toFixed(1) || 'No data',
-                    min_temperature: +data[sol_key].AT?.mn.toFixed(1) || 'No data',
+                    max_temperature: data[sol_key].AT?.mx.toFixed(1) || 'No data',
+                    min_temperature: data[sol_key].AT?.mn.toFixed(1) || 'No data',
                     avg_temperature: Math.round(data[sol_key].AT?.av * 10) / 10 || 'No data',
                     max_pressure: Math.round(data[sol_key].PRE?.mx * 10) / 10 || 'No data',
                     min_pressure: Math.round(data[sol_key].PRE?.mn * 10) / 10 || 'No data',
                     avg_pressure: Math.round(data[sol_key].PRE?.av * 10) / 10 || 'No data',
                     wind_speed: Math.round(data[sol_key].HWS?.av * 10) / 10 || 0,
-                    wind_direction: data[sol_key].WD?.most_common.compass_point || 0,
-                    wind_degrees: data[sol_key].WD?.most_common.compass_degrees || 0,
+                    wind_direction: data[sol_key].WD?.most_common?.compass_point || 0,
+                    wind_degrees: data[sol_key].WD?.most_common?.compass_degrees || 0,
                     season: data[sol_key].Season,
                     date_UTC: data[sol_key].First_UTC,
                     date_string: `${month} ${day}`
